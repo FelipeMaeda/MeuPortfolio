789df91e3a8647a21cf63a1900791013f70fb717
FelipeMaeda <56806716+FelipeMaeda@users.noreply.github.com>
2021-02-27 06:17:33 -0300

    Update Ccard

--- meu-portfolio/components/Content/Card.js
+++ meu-portfolio/components/Content/Card.js
@@ -8,10 +8,11 @@ export default function CardContent() {
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="holder.js/100px180" />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title>Projeto 1</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            PRojeto 1?
+                            Lorem Ipsolum at
+                            Adisdo Nuyit nu on
                         </Card.Text>
                         <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
--- meu-portfolio/pages/index.js
+++ meu-portfolio/pages/index.js
@@ -6,6 +6,9 @@ export default function Home() {
     <>
         <NavBarMenu />
         <Card />
+        <Card />
+        <Card />
+        <Card />
     </>
   )
 }
