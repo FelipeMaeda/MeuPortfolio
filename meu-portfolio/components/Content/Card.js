fa5ffe9d98c258ae7e2b1754ff4aa71d58131ca8
FelipeMaeda <56806716+FelipeMaeda@users.noreply.github.com>
2021-02-27 05:58:17 -0300

    Update Card.js

--- meu-portfolio/components/Content/Card.js
+++ meu-portfolio/components/Content/Card.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Container } from 'react-bootstrap';
 
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-export default Card;
+export default function CardContent() {
+    return (
+        <div style={{ background: 'linear-gradient(#f8f9fa, #fff)' }}>
+            <Container>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </Container>
+        </div>
+    )
+}
